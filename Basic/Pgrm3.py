3712d2e892803be86233a6c64fe9d0a5eb33d471
Anshumaan09 <anshumaan342@gmail.com>
2026-08-05 17:55:55 +0530

    uploaded more program

--- Basic/Pgrm3.py
+++ Basic/Pgrm3.py
@@ -9,3 +9,22 @@ for i in range(1,6):
     print()
 
 num1 = int(input("Enter a number: "))
+for i in range(num1, 0, -1):
+    print(i)
+
+
+num2 = int(input("Enter a number: "))
+count = 0
+while num2 > 0:
+    num2 //= 10
+    count += 1
+print("The number of digits in the number is:", count)
+
+
+num3 = int(input("Enter a number: "))
+total = 0
+while num3 > 0:
+    digit = num3 % 10
+    total += digit
+    num3 //= 10
+print(total)
