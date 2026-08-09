27c299e1ec1fcd25efdb0446ccfdd2e1b822b5a5
Anshumaan09 <anshumaan342@gmail.com>
2026-08-09 09:12:41 +0530

    Three Sum

---
+++ 3Sum.py
@@ -0,0 +1,26 @@
+def threeSum(nums: list[int]) -> list[list[int]]:
+    nums.sort()
+    result = []
+    for i in range(len(nums)-2):
+        if i>0 and nums[i] == nums[i-1]:
+            continue
+        left = i+1
+        right = len(nums) - 1
+        while left < right:
+            sum_value = nums[i] + nums[left] + nums[right]
+            if sum_value == 0:
+                result.append([nums[i],nums[left],nums[right]])
+                left += 1
+                right -= 1
+                while left < right and nums[left] == nums[left-1]:
+                    left +=1
+                while left < right and nums[right] == nums[right + 1]:
+                    right -=1
+            elif sum_value < 0:
+                left += 1
+            else:
+                right -=1
+    return result
+
+arr = [-1,0,1,2,-1,-4] # [-4,-1,-1,0,1,2]
+print("3sum",threeSum(arr))
