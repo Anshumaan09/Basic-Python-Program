a2967d8bd1ebfa097a1843591fa19450e0731d7f
Anshumaan09 <anshumaan342@gmail.com>
2026-08-21 00:29:04 +0530

    Doubble Linked List

---
+++ Linked_List/DoubleLL.py
@@ -0,0 +1,90 @@
+# Here we can traverse in both direction as there will be forward address and previous address
+# 1st Node -> prev = None, data and Next = temp.prev
+# 2nd Node -> prev = head.next , temp = data, Next = None
+
+class Node:
+    def __init__(self, value = None):
+        self.prev = None
+        self.data = value
+        self.next = None
+    
+class DoublyLL:
+    def __init__(self):
+        self.head = None
+    
+    def insertAtEnd(self, value):
+        temp = Node(value)
+        if self.head == None:
+            self.head = temp
+            return
+        
+        t = self.head
+        while t.next != None:
+            t = t.next
+        
+        t.next = temp
+        temp.prev = t
+        
+    def insertAtBeg(self,value):
+        temp = Node(value)
+        if self.head == None:
+            self.head = temp
+        
+        # save the node
+        temp.next = self.head
+        self.head.prev = temp
+        self.head = temp
+        
+    def insertAtMid(self, value, x):
+        t = self.head
+        while t.next != None:
+            if t.data == x:
+                break
+            else:
+                t = t.next
+                
+        temp = Node(value)      
+        temp.next = t.next
+        t.next.prev = temp
+        t.next = temp
+        temp.prev = t
+        
+    def deletionDLL(self, value):
+        if(self.head == None):
+            print("Linked List is empty")
+            return
+        
+        t = self.head
+        if t.data == value:
+            self.head = t.next
+            self.head.prev = None
+            return
+            
+        while t.next != None:
+            if(t.data == value):
+                t.prev.next = t.next
+                t.next.prev = t.prev
+                return
+            else:
+                t = t.next
+        
+        if t.data == value:
+            t.prev.next = None
+                
+        
+    def printDLL(self):
+        t = self.head
+        while t.next != None:
+            print(t.data, end = "<-->")
+            t = t.next
+        print(t.data)
+
+obj = DoublyLL()
+obj.insertAtEnd(10)
+obj.insertAtEnd(20)
+obj.insertAtEnd(30)
+obj.insertAtEnd(40)
+obj.insertAtBeg(5)
+obj.insertAtMid(50,20)
+obj.deletionDLL(40)
+obj.printDLL()
